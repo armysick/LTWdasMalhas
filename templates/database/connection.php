3a0004561df31566572258c5ec144a7dcc9a15d1
killghost15 <chrzzz@hotmail.com>
2015-12-04 05:38:43 +0000

    tinha uma cena marada q nem vale a pena contar

--- templates/database/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-// get a database connection (not persistent)
-$db=new PDO('sqlite:DB.db');
-
-?>
--- templates/templates/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="mainFooter">
-    	<p>Copyright &copy; 2015 <a href="#" title="manage_events">manage events</a></p>
-    </footer>
-</body>
-</html>
