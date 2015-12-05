e43831c3274892d5359aee286e0174f808a32814
paulobfsilva <paulobfsilva@gmail.com>
2015-12-05 23:36:38 +0000

    $_SESSION contains admin information

--- SignIn.php
+++ SignIn.php
@@ -16,7 +16,7 @@
 			
 			$username=$_POST['user'];
 			$password=$_POST['pass'];
-			$stmt =$db->prepare('SELECT password,salt,idUser FROM Users WHERE username = :user LIMIT 1');
+			$stmt =$db->prepare('SELECT password,salt,idUser,admin FROM Users WHERE username = :user LIMIT 1');
 			$stmt->bindParam(':user', $username);
 			$stmt->execute();
 			$chiclas = $stmt->fetch();
@@ -27,8 +27,9 @@
 			if($pwdhash == $chiclas['password']){
 				$_SESSION['id_user']=$chiclas['idUser'];
 				$_SESSION['login_user'] = $username;            	// store the username
+				$_SESSION['admin'] = $chiclas['admin'];				//to check if user is admin or not
 				echo'<script language="javascript">';
-				echo 'alert("Correct username and/or password.")';
+				echo 'alert("Correct username and/or password")';
 				echo '</script>';
 				$redirectUrl = 'profile.php';
 				echo '<script type="application/javascript">window.location.href = "'.$redirectUrl.'";</script>';
