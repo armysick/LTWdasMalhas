64fb3ddc9063bd14b9c267b592d07175636f5858
armysick <uh4v3b33nh4ck3d@gmail.com>
2015-12-06 18:10:52 +0000

    Ev_cr correc. Del_ev delets Comm+Regis;

--- create_event.php
+++ create_event.php
@@ -84,7 +84,7 @@ if ($uploadOk == 0) {
 	$stmt2->bindParam(':event_date',$E_date);
 	$stmt2->bindParam(':public',$Public);	
 	if($stmt->execute()){
-		if($public){
+		if($Public){
 		$stmt2->execute();
 		$idEve = $stmt2->fetch();
 		
@@ -97,7 +97,7 @@ if ($uploadOk == 0) {
 		
 		$link_new = $xml->addChild('link');
 		
-		$link_new->addChild('url', $str);
+		$link_new->addChild('title', $Description);
 		$str = '/event_page.php?id='.$idEve['idEvent'];
 		$link_new->addChild('url', $str);
 		$xmlFormat = $xml->asXML();
--- delete_event.php
+++ delete_event.php
@@ -9,9 +9,57 @@
 
     $event_id_page = $_GET["event_id"];
 	
+	$stmtd = $db->prepare('SELECT description FROM Events WHERE idEvent = :id_event');
+	$stmtd->bindParam(':id_event', $event_id_page);
+	$stmtd->execute();
+	$ev_description = $stmtd->fetch();
+	
     $stmt =$db->prepare('DELETE FROM Events WHERE idEvent= :id_event');
     $stmt->bindParam(':id_event',$event_id_page);
     $stmt->execute();
+	
+	// Remove XML element
+	
+	
+		$file = 'links.xml';
+		
+		$xml = simplexml_load_file($file);
+		$xmlFormat = $xml->asXML();
+		foreach ($xml as $links) {
+			// debug echo '<br><br\>';
+			// debug echo $links->title;
+			if ($links->title == $ev_description['description']) {
+				$dom = dom_import_simplexml($links);
+				$dom->parentNode->removeChild($dom);
+				$fp = fopen("links.xml","wb");
+				fwrite($fp,$xml->asXML());
+				fclose($fp);
+			}
+		}
+		
+		
+		//REMOVE All REgisters from Event
+	$stmtreg = $db->prepare('SELECT idRegister FROM Registers WHERE idEvent = :id_event');
+	$stmtreg->bindParam(':id_event',$event_id_page);
+	$stmtreg->execute();
+	$stmtreg->fetch();
+	foreach ($stmtreg as $res){
+		$stmtrem=$db->prepare('DELETE FROM Registers WHERE idRegister = :id_register');
+		$stmtrem->bindParam(':id_register', $res['idRegister']);
+		$stmtrem->execute();
+	}
+	
+	//REMOVE All Comments from Event
+	$stmtcom = $db->prepare('SELECT idComment FROM Comments WHERE idEvent = :id_event');
+	$stmtcom->bindParam(':id_event',$event_id_page);
+	$stmtcom->execute();
+	$stmtcom->fetch();
+	foreach ($stmtcom as $resc){
+		$stmtremc=$db->prepare('DELETE FROM Comments WHERE idComment = :id_comment');
+		$stmtremc->bindParam(':id_comment', $resc['idComment']);
+		$stmtremc->execute();
+	}
+	
     $redirectUrl = 'manage_events.php';
 	echo '<script type="application/javascript">window.location.href = "'.$redirectUrl.'";</script>';
 ?>
